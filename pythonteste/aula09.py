929118bdc8cf0fe9fcda913bd4cdce0f6c1c9331
Mariana M.C <marianamoioli@gmail.com>
2023-03-20 09:36:49 -0300

    add exercises

---
+++ pythonteste/aula09.py
@@ -0,0 +1,13 @@
+frase = 'Curso em Vídeo Python'
+print(frase[3])
+print(frase[3:13:2])
+print(frase[0:14])
+
+print(frase.upper().count('o'))
+print(len(frase))
+print(frase.replace('Python', 'Androide'))
+print('Curso' in frase)
+
+print(frase.split())
+
+
