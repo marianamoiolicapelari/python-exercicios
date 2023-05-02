bb46dfb08132eb1cd258b2edca87a8369bf3d6ae
Mah Moioli Capelari <100173586+marianamoiolicapelari@users.noreply.github.com>
2023-05-02 07:28:09 -0300

    Update ex022.py

--- PythonExercicios/ex022.py
+++ PythonExercicios/ex022.py
@@ -3,7 +3,9 @@ print('Analisando seu nome...')
 print('Seu nome em maiúsculas é {}' .format(nome.upper()))
 print('Seu nome em minúsculas é {}'.format(nome.lower()))
 print('Seu nome têm ao todo {} letras'.format(len(nome) - nome.count(' ')))
+
 #print('Seu primeiro nome tem {} letras '.format(nome.find(' ')))
+
 separa = nome.split()
 print('Seu primeiro nome é {} e ele têm {} letras'.format(separa[0], len(separa[0])))
 
